c1ae198aa3d5d698d33878b89b5621e250b37da0
johan52824 <58148047+johan52824@users.noreply.github.com>
2022-05-16 23:05:27 -0500

    Update MainActivity.java

--- facci.erick johan mera espinal pp/app/src/main/java/com/example/tareaindivualerick/MainActivity.java
+++ facci.erick johan mera espinal pp/app/src/main/java/com/example/tareaindivualerick/MainActivity.java
@@ -1,18 +1,64 @@
 package com.example.tareaindivualerick;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
 import android.util.Log;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Log log;
+    EditText txtFahrenheitClase;
+    EditText txtCentigradosClase;
+    Button buttonConversionCenti;
+    Button buttonConversionFahre;
+    Button btnBorrado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        log.e("aplicaion movil 1","erick johan mera espinal ");
+        Log.d(" CONVERTIDOR ", "ERICK JOHAN MERA ESPINAL");
+        buttonConversionCenti = (Button) findViewById(R.id.buttonConversionC);
+        buttonConversionFahre = (Button) findViewById(R.id.buttonConversionF);
+        btnBorrado = (Button) findViewById(R.id.btnBorrar);
+        txtFahrenheitClase = (EditText) findViewById(R.id.txtFahrenheit);
+        txtCentigradosClase = (EditText) findViewById(R.id.txtCentigrados);
+
+        buttonConversionCenti.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float valor = Float.parseFloat(txtFahrenheitClase.getText().toString());
+                txtCentigradosClase.setText(String.valueOf(ConverterUtil.convertFahrenheit_Celsius(valor)));
+
+            }
+        });
+        buttonConversionFahre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float valor2 = Float.parseFloat(txtCentigradosClase.getText().toString());
+                txtFahrenheitClase.setText(String.valueOf(ConverterUtil.convertCelsius_Fahrenheit(valor2)));
+            }
+        });
+        btnBorrado.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtFahrenheitClase.setText("");
+                txtCentigradosClase.setText("");
+            }
+        });
+
     }
+
+    public static class ConverterUtil {
+
+        public static float convertFahrenheit_Celsius(float fahrenheit) {
+            return ((fahrenheit - 32) * 5 / 9);
+        }
+
+        public static float convertCelsius_Fahrenheit(float celsius) {
+            return ((celsius * 9) / 5) + 32;
+        }
+    }
+
 }
